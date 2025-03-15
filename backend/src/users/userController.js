ee970d079af90dbf24aec5b6a2b393da023b2e94
debjyoti sarkar <debjyotisarkar929@gmail.com>
2025-03-15 19:48:40 +0530

    login implemented

--- backend/src/users/userController.js
+++ backend/src/users/userController.js
@@ -70,5 +70,60 @@ const createUser = async (req, res, next) =>{
 }
 
 
+const loginUser = async (req, res, next) =>{
 
-export {createUser}
+        const {email,password} = req.body;
+        if(!email || !password){
+            const error = createHttpError(400, "All fields are required");
+            return next(error); // passing err to global err handler  to client 
+        }
+    //check user in db or not
+
+    let user 
+    try {
+        user = await userModel.findOne({email});
+        if(!user){
+            const error = createHttpError(404, "User not found");
+            return next(error); // passing err to global err handler  to client 
+        }
+    } catch (error) {
+        return next(createHttpError(500,"error while getting user"));   
+    }
+    //check password
+    let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password,user.password);
+    if(!isValidPassword){
+        const error = createHttpError(401, "incorrect password");
+        return next(error); // passing err to global err handler  to client
+      }
+  } catch (error) {
+    return next(createHttpError(500,"error while comparing password"));   
+  }
+
+
+    //handle err? 
+    try {
+            //create access token
+    // token generation --> sub is the payload which is the user id and swcond param is the secret
+ 
+        const token = sign({sub:user._id},
+            config.jwtSecret ,{
+            expiresIn:"7d",
+            algorithm:"HS256"
+            }
+        )
+        
+            res.json({token});
+    } catch (err) {
+        return next(createHttpError(500,"error while creating token"));
+    }
+
+
+
+
+}
+
+
+
+export {createUser, loginUser}
