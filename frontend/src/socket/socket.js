5b7502fbbdaffdd4ba85468d78bd5927a69d11fb
debjyoti sarkar <debjyotisarkar929@gmail.com>
2025-03-16 18:55:32 +0530

    auctions byid route

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,7 @@
+import { useMemo } from "react";
 import { io } from "socket.io-client";
 
-const socket=io("http://localhost:3300");
+const socket=useMemo(() => io("http://localhost:8000"), []);
 
 
 export const socketConnect=()=>{
