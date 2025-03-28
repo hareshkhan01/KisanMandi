7456a590e26aceac1e669c4101c4078297f8ca98
debjyoti sarkar <debjyotisarkar929@gmail.com>
2025-03-28 21:06:44 +0530

    add twilioservice

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,8 +4,14 @@ conf();
 const _config = {
   mongoString: process.env.MONGO_STRING,
   port: process.env.PORT,
-  jwtSecret: process.env.JWT_SECRET
+  jwtSecret: process.env.JWT_SECRET,
+  twilioSid: process.env.TWILIO_ACCOUNT_SID,
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioNumber: process.env.SANDBOX_NUMBER
+
 };
 
-
+// console.log('====================================');
+// console.log(_config);
+// console.log('====================================');
 export const config = Object.freeze(_config);
---
+++ backend/src/middlewares/twilioService.js
@@ -0,0 +1,20 @@
+import { config } from 'dotenv';
+import twilio from 'twilio'
+
+const client = twilio(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN);
+
+const sendSMS = async (number, message) => {
+    try {
+        const res = await client.messages.create({
+            body: message,
+            from:config.TWILIO_NUMBER,
+            to: number
+        });
+        console.log(res.sid);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default sendSMS;
